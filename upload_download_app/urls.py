c06073b6c0fef983af0fc4d5def63223d0dddf66
hamed6 <h.ghodsinezhad@gmail.com>
2022-02-01 21:26:05 +0200

    endpoints

--- upload_download_app/urls.py
+++ upload_download_app/urls.py
@@ -9,3 +9,11 @@ urlpatterns=[
     # path('api/upload/<str:uid>', views.UploadFile, name='uploadfile')
     url('upload/',UploadFile, name='fileupload)')
 ]
+
+
+# /api/upload/uid/'uploadfile'
+# /api/viewfilelist/uid
+# /api/download/uid/filenumber: add +1 to dl column of file
+
+# /api/filestatistic: list of file with number of dl in front of that
+# /api/orgstatistic: list of org with number of dl in front of it
